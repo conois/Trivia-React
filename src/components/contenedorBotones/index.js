c3316afe01252d96da2eef4d75acec2676dfcd76
camilabelen <ccornejoss@gmail.com>
2018-03-14 10:03:02 -0300

    componentes botones y titulo

---
+++ src/components/contenedorBotones/index.js
@@ -0,0 +1,5 @@
+import React,{ Component } from 'react';
+import BotonEasy from './botones';
+import BotonMedium from './botones';
+import BotonHard from '.botones';
+import styles from './styles.css'
